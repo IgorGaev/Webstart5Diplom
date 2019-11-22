4f78cff070f75614c6c8236e6679d6ba8330650c
Igor Gaev <io.gaev@gmail.com>
2019-11-22 21:20:04 +0500

    2 проверка

---
+++ dist/js/slider.js
@@ -0,0 +1,2 @@
+
+$(document).ready(function(){$('.hero-slider').slick({autoplay:1,dots:true,arrows:false,autoplaySpeed:4000,dotsClass:"hero__dots"});$('.reviews-slider').slick({dots:true,arrows:false,dotsClass:"reviews__dots",slidesToShow:2,slidesToScroll:1,responsive:[{breakpoint:576,settings:{slidesToShow:1,slidesToScroll:1}}]});})
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,27 @@
 $(document).ready(function () {
   $('.hero-slider').slick({
-    autoplay: 0,
+    autoplay: 1,
     dots: true,
     arrows: false,
     autoplaySpeed: 4000,
     dotsClass: "hero__dots"
   });
+  
+
   $('.reviews-slider').slick({
-    autoplay: 0,
     dots: true,
     arrows: false,
-    dotsClass: "reviews__dots"
+    dotsClass: "reviews__dots",
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
 })
